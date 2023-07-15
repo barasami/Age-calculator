39cc5a1c8bb116401f1161c4d0578a308a0ad56b
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-07-15 21:29:27 +0300

    fixed data

--- index.js
+++ index.js
@@ -1,5 +1,27 @@
+//year
+//month
+//day
+
+
 let btn=document.querySelector('.btn')
+let sirName=document.querySelector('.sir')
+let month=document.querySelector('.monthdata')
+let date=document.querySelector('.date')
+
+
+
+let today=new Date()
+let tod=new Intl.DateTimeFormat('en-us',{
+    dateStyle:'long'
+})
+
+let todaysDate=(tod.format(today))
+console.log(todaysDate);
 
 btn.addEventListener('click',()=>{
-    console.log('hello there');
+    console.log(sirName.value);
+    console.log(month.value);
+    console.log(date.value);
 })
+
+
