86114f676cf19fa272e222185db3829079768d6e
Barasa O Micheal <46565582+barasami@users.noreply.github.com>
2023-07-16 21:38:40 +0300

    fixed data

--- index.js
+++ index.js
@@ -7,7 +7,10 @@ let btn=document.querySelector('.btn')
 let sirName=document.querySelector('.sir')
 let month=document.querySelector('.monthdata')
 let date=document.querySelector('.date')
+let years=document.querySelector('.years')
+let old=document.querySelector('.old')
 
+let allToday=document.getElementById("today")
 
 
 let today=new Date()
@@ -15,13 +18,69 @@ let tod=new Intl.DateTimeFormat('en-us',{
     dateStyle:'long'
 })
 
+let allyear=today.getFullYear()
+let alltoday=today.getMonth()+1
+console.log(alltoday);
+
 let todaysDate=(tod.format(today))
-console.log(todaysDate);
+
+
+allToday.innerHTML=`${todaysDate}`
+
+
+
+
 
 btn.addEventListener('click',()=>{
-    console.log(sirName.value);
-    console.log(month.value);
-    console.log(date.value);
+
+    //get Name
+    let sName=sirName.value
+    if(sName === '' || sName === null){
+        return
+    }
+
+    //get Month
+    let sMonth=month.value
+    if(sMonth === '' || sMonth === null){
+        return
+    }
+    else if(sMonth < 1 || sMonth > 12){
+        return
+    }
+    else{
+        let getcalcYear=Number((sMonth.slice(0,4)));//year
+        let getcalcMonth=Number((sMonth.slice(5,7)));//month
+
+        console.log(getcalcMonth);
+    }
+    
+
+    //get Date
+    let sDate=date.value
+    if(sDate === '' || sDate === null){
+        return
+    }
+    else if(sDate < 1 || sDate > 31){
+        return
+    }
+    else{
+        console.log(sDate);
+    }
+    
+
+    //get Year
+    let syear=years.value
+    if(syear === '' || syear > allyear){
+        return
+    }
+    // console.log(syear);
+
+    let calcYears=allyear-syear
+
+
+    old.innerHTML=`Hi ${sName} you are ${calcYears} Years Old`
+
+    
 })
 
 
